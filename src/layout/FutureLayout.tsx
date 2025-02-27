d795c6411c084218b58cd3ace12537b4a669b445
stealthscripter <natnael01ayele@gmail.com>
2025-02-27 14:39:12 +0300

    public assets

--- src/layout/FutureLayout.tsx
+++ src/layout/FutureLayout.tsx
@@ -5,7 +5,7 @@ function FutureLayout() {
   return (
     <div
       className="p-4 md:grid md:grid-cols-9 gap-x-2 md:min-h-screen border h-screen overflow-hidden border-amber-900
-    bg-[url(src/assets/backgrounds/stacked-steps-haikei.svg)] bg-slate-900 bg-blend-difference"
+    bg-[url(/assets/backgrounds/stacked-steps-haikei.svg)] bg-slate-900 bg-blend-difference"
       style={{ backgroundRepeat: "no-repeat", backgroundSize: "cover" }}
     >
       <GameHeader />
